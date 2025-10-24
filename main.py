38e24b78f50641296907f27c0d01b562ee2a3a7b
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-10-24 20:48:47 +0530

    Added load_data function and view_data endpoint to main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    return data
+
 @app.get("/")
 async def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Patients Mangement System API"}
 
 @app.get("/about")
 async def read_about():
-    return {"message": "This is the about page"}
+    return {"message": "A fully functional API for managing patients' data."}
+
+@app.get("/view")
+async def view_data():
+    data = load_data()
+    return {"data": data}
