9a44c4cd23e1db0bc089b8f5674c6e0c9314f478
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-04 07:57:44 +0530

    `Added function to update patient data and updated patient instance creation.`

--- concepts/2_use_pydantic.py
+++ concepts/2_use_pydantic.py
@@ -10,11 +10,22 @@ def insert_patient_data(patient: Patient):
     print(f"Inserting patient data: Name = {patient.name}, Age = {patient.age}")
     print("Patient data inserted successfully.")
 
+#function to update patient data
+def update_patient_data(patient: Patient):
+    print(f"Updating patient data: Name = {patient.name}, Age = {patient.age}")
+    print("Patient data updated successfully.")
+
 #creating patient instance
-patient_info = {'name': 'piyush', 'age': 30}
+patient_info = {'name': 'piyush', 'age': 21}
+patient1_info = {'name': 'pankaj', 'age': 20}
 
 #validating and creating Patient object
 patient1 = Patient(**patient_info)
+patient2 = Patient(**patient1_info)
 
 #inserting patient data  
 insert_patient_data(patient1)
+
+#updating patient data
+update_patient_data(patient2)
+update_patient_data(Patient(name='rahul', age=22))
