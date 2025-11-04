b0e926a7f94daf482615459c6b4386f9710cd24e
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-04 08:12:42 +0530

    `Added fields to Patient model and updated example usage`

--- concepts/2_use_pydantic.py
+++ concepts/2_use_pydantic.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel
+from typing import List, Dict
 
 #defining Patient model
 class Patient(BaseModel):
     name: str
     age: int
+    weight: float
+    isMarried: bool = False
+    allergies: list[str]
+    contact_details: Dict[str, str]
+    fees: float = 500.0
 
 #function to insert patient data
 def insert_patient_data(patient: Patient):
-    print(f"Inserting patient data: Name = {patient.name}, Age = {patient.age}")
+    print(f"Inserting patient data: Name = {patient.name}, Age = {patient.age}, Weight = {patient.weight}, IsMarried = {patient.isMarried}, Allergies = {patient.allergies}, Contact Details = {patient.contact_details} , Fees = {patient.fees}")
     print("Patient data inserted successfully.")
 
 #function to update patient data
@@ -16,16 +22,13 @@ def update_patient_data(patient: Patient):
     print("Patient data updated successfully.")
 
 #creating patient instance
-patient_info = {'name': 'piyush', 'age': 21}
-patient1_info = {'name': 'pankaj', 'age': 20}
+patient_info = {'name': 'rohit', 'age': 21, 'weight': 70.5, 'isMarried': False, 'allergies': ['pollen', 'nuts'], 'contact_details': {'email': 'rohit@example.com', 'phone': '123-456-7890'}, 'fees': 1580.00}
 
 #validating and creating Patient object
 patient1 = Patient(**patient_info)
-patient2 = Patient(**patient1_info)
 
 #inserting patient data  
 insert_patient_data(patient1)
 
 #updating patient data
-update_patient_data(patient2)
-update_patient_data(Patient(name='rahul', age=22))
+# update_patient_data(Patient(name='rahul', age=22))
