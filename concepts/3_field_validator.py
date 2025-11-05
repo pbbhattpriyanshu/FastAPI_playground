d0b98611df295956846028ea96e79b119c3639da
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-05 23:12:56 +0530

    creating patient instance

--- concepts/3_field_validator.py
+++ concepts/3_field_validator.py
@@ -32,3 +32,16 @@ class Patient(BaseModel):
 def insert_patient_data(patient: Patient):
     print(f"Inserting patient data: Name = {patient.name}, Age = {patient.age}, Weight = {patient.weight}, Allergies = {patient.allergies}, IsMarried = {patient.isMarried}, IsFamilyMember = {patient.isFamilyMember}, Email = {patient.email}, LinkedIn = {patient.linkedIn}, Contact Details = {patient.contact_details} , Fees = {patient.fees}")
     print("Patient data inserted successfully.")
+
+#creating patient instance
+patient_info = {
+    'name': 'rohit', 
+    'age': 21, 
+    'weight': 70.5, 
+    'isMarried': False, 
+    'isFamilyMember': True, 
+    'allergies': ['pollen', 'nuts'], 
+    'email': 'rohit@example.com', 
+    'linkedIn': 'https://www.linkedin.com/in/rohit', 
+    'contact_details': {'address': '542 A Mark Street Market, Near grandson Park', 'phone': '123-456-7890'},
+    'fees': 1580.00}
