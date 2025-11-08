75b6d44dfcd8da7cf30fc98c2529ea78b19024cb
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-08 23:40:06 +0530

    `Added API endpoints for patient management system`

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
+# Initialize FastAPI app
 app = FastAPI()
 
+# Load data from JSON file
 def load_data():
     with open('data.json', 'r') as file:
         data = json.load(file)
     return data
 
+# Define API endpoints
 @app.get("/")
 async def read_root():
     return {"message": "Patients Mangement System API"}
 
+# About endpoint
 @app.get("/about")
 async def read_about():
     return {"message": "A fully functional API for managing patients' data."}
 
+# Status endpoint
 @app.get("/status")
 async def read_status():
     return {"message": "There is no such booking, regarding this application"}
 
+# View data endpoint
 @app.get("/view")
 async def view_data():
     data = load_data()
     return {"data": data}
 
+# Get patient by ID endpoint
 @app.get("/patient/{patient_id}")
 async def get_patient(patient_id: str = Path(..., description="The ID of the patient to retrieve", example= "P001")):
     # Load data from JSON file
@@ -35,6 +42,7 @@ async def get_patient(patient_id: str = Path(..., description="The ID of the pat
     else:
         raise HTTPException(status_code=404, detail="Patient not found")
 
+# Sort patients endpoint
 @app.get("/sort")
 def sort_patients(sort_by: str = Query(..., description="The field to sort patients on the basis of height, weight or bmi"), order: str = Query("asc", description="The order of sorting: asc for ascending, desc for descending")):
     valid_sort_fields = {"height", "weight", "bmi"}
@@ -52,3 +60,4 @@ def sort_patients(sort_by: str = Query(..., description="The field to sort patie
     sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=reverse)
 
     return {"sorted_data": sorted_data}
+
